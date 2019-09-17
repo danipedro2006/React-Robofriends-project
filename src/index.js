07e0706af9df614df61473b9b84035ec4bf8fc0b
danipedro2006 <danipedro2006@yahoo.com>
2019-09-17 09:20:12 +0300

    starting new react project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Card.js';
+import CardList from './CardList';
+import 'tachyons';
+import {robots} from './robots';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <CardList robots ={robots} />
+
+  , document.getElementById('root'));
+
+serviceWorker.unregister();
